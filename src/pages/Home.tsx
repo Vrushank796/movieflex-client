37da9f522588c1b1f50e7b4aaa4f4fc1c684ac4d
Vrushank796 <vrushank796@gmail.com>
2024-07-30 19:27:47 -0400

    Notification for server startup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import '../assets/stylesheets/Homepage.css';
-import CardGrid from '../components/CardGrid';
+import React, { useEffect } from "react";
+import "../assets/stylesheets/Homepage.css";
+import CardGrid from "../components/CardGrid";
 
 type HomeProps = {};
 
 const Home = (props: HomeProps) => {
+  useEffect(() => {
+    alert(
+      "Please refresh the page if movies take time to load, as our free hosting service may delay server startup."
+    );
+  });
+
   return (
-    <div className='container-fluid main-homepage'>
-      <div className='row home-title'>
-        <h1 className='page-title ml-4 mt-4'>New Releases</h1>
+    <div className="container-fluid main-homepage">
+      <div className="row home-title">
+        <h1 className="page-title ml-4 mt-4">New Releases</h1>
       </div>
-      <div className='row new-released'>
+      <div className="row new-released">
         <CardGrid />
       </div>
     </div>
